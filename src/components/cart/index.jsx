68f679b7a5da3e3f80bb198cb7298e911e6f12bc
sandunrmst <sandunalugc@gmail.com>
2024-10-15 10:02:12 +0530

    navigate to cart

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,6 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
 function CartIndicater() {
+  const navigate = useNavigate();
+
+  const handleCartNavigation = () => {
+    navigate("/cart");
+  };
+
   return (
-    <div className="w-full max-w-[150px] flex items-center flex-col flex-shrink border-2 px-1 py-4  rounded-[100px] border-cyan-700 cursor-pointer">
+    <div
+      onClick={handleCartNavigation}
+      className="w-full max-w-[150px] flex items-center flex-col flex-shrink border-2 px-1 py-4  rounded-[100px] border-cyan-700 cursor-pointer"
+    >
       <h2 className="text-2xl font-bold text-cyan-700">Cart</h2>
       <p className="font-semibold">2 Items</p>
       <p>$ 65</p>
