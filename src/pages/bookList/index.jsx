7c81954e35eb99b7b45a10e9b0318647fc451f60
sandunrmst <sandunalugc@gmail.com>
2024-10-15 09:45:47 +0530

    create book listing page

--- src/pages/bookList/index.jsx
+++ src/pages/bookList/index.jsx
@@ -1,5 +1,64 @@
+import { useEffect, useState } from "react";
+import BookCard from "../../components/bookCard";
+import Search from "../../components/search";
+import CartIndicater from "../../components/cart";
+
 function BookListPage() {
-  return <div>Bool List Page</div>;
+  const [bookList, setBookList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  async function fetchBookList() {
+    setLoading(true);
+    const list = await fetch("https://api.itbook.store/1.0/new");
+    const bookList = await list.json();
+
+    if (bookList) {
+      setLoading(false);
+      setBookList(bookList.books);
+    }
+  }
+
+  useEffect(() => {
+    fetchBookList();
+  }, []);
+
+  if (loading)
+    return (
+      <>
+        <div className="bg-cyan-700 py-4">
+          <h1 className="text-4xl font-bold text-gray-100 text-center">
+            Book Store
+          </h1>
+        </div>
+        <div className="text-2xl font-semibold text-center mt-10">
+          Loading...Please wait!
+        </div>
+        ;
+      </>
+    );
+  return (
+    <>
+      <div className="bg-cyan-700 py-4">
+        <h1 className="text-4xl font-bold text-gray-100 text-center">
+          Book Store
+        </h1>
+      </div>
+      <div className="max-w-6xl mx-auto p-4 ">
+        <div className="flex w-full gap-6 justify-end p-4 items-center">
+          <Search />
+          <CartIndicater />
+        </div>
+        <h2 className="text-2xl font-bold text-cyan-700 mb-5">New Books</h2>
+        <div className="min-h-[80vh] grid sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-2 max-w-6xl mx-auto gap-y-10 gap-x-12">
+          {bookList && bookList.length
+            ? bookList.map((bookItem, index) => (
+                <BookCard key={index} bookItem={bookItem} />
+              ))
+            : null}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BookListPage;
