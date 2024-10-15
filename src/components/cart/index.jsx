5b2cb00ab763e414309af1d584050971bfb6c645
sandunrmst <sandunalugc@gmail.com>
2024-10-15 19:42:03 +0530

    set header cart

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 function CartIndicater() {
@@ -7,14 +9,27 @@ function CartIndicater() {
     navigate("/cart");
   };
 
+  const [total, setTotal] = useState(0);
+
+  const { cart } = useSelector((state) => state);
+
+  useEffect(() => {
+    setTotal(
+      cart.reduce((acc, current) => {
+        const removeDollar = parseFloat(current.price.replace("$", ""));
+        return acc + removeDollar;
+      }, 0)
+    );
+  }, [cart]);
+
   return (
     <div
       onClick={handleCartNavigation}
       className="w-full max-w-[150px] flex items-center flex-col flex-shrink border-2 px-1 py-4  rounded-[100px] border-cyan-700 cursor-pointer"
     >
       <h2 className="text-2xl font-bold text-cyan-700">Cart</h2>
-      <p className="font-semibold">2 Items</p>
-      <p>$ 65</p>
+      <p className="font-semibold">{cart.length} Items</p>
+      <p>$ {total.toFixed(2)}</p>
     </div>
   );
 }
