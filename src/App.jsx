5d593b433ed7e9bf7a034a50d71cfa4727d159be
sandunrmst <sandunalugc@gmail.com>
2024-10-15 09:50:21 +0530

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import BookDetailsPage from "./pages/bookDetails";
+import BookListPage from "./pages/bookList";
+import CartListPage from "./pages/cartList";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<BookListPage />} />
+        <Route path="/product-details/:id" element={<BookDetailsPage />} />
+        <Route path="/cart" element={<CartListPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
