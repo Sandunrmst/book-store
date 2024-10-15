76ee893798fce0abe1dea2684ac8f258e16c7418
sandunrmst <sandunalugc@gmail.com>
2024-10-15 11:04:11 +0530

    add dynamic route

--- src/components/bookCard/index.jsx
+++ src/components/bookCard/index.jsx
@@ -1,40 +1,44 @@
+import { Link } from "react-router-dom";
+
 function BookCard({ bookItem }) {
   return (
-    <div className="w-full flex justify-evenly  border-2 rounded-md border-cyan-700 p-2 shadow-md gap-2">
-      <div className="flex-1">
-        <img
-          src={bookItem?.image}
-          alt={bookItem?.title}
-          className="object-cover w-full"
-        />
+    <Link to={`/product-details/${bookItem?.isbn13}`}>
+      <div className="w-full flex justify-evenly  border-2 rounded-md border-cyan-700 p-2 shadow-md gap-2">
+        <div className="flex-1">
+          <img
+            src={bookItem?.image}
+            alt={bookItem?.title}
+            className="object-cover w-full"
+          />
+        </div>
+        <div className="flex-1">
+          <h2>
+            <span className="font-bold">Title:</span> {bookItem?.title}
+          </h2>
+          <h3>
+            <span className="font-bold">Subtitle:</span>
+            {bookItem?.subtitle}
+          </h3>
+          <p>
+            <span className="font-bold">ISBN:</span> {bookItem?.isbn13}
+          </p>
+          <p>
+            <span className="font-bold">Price:</span> {bookItem?.price}
+          </p>
+        </div>
+        <div className="flex flex-col items-end">
+          <input
+            className="border-2 border-cyan-700 rounded-md w-14 text-slate-950"
+            type="number"
+            name="cartcounter"
+            min="1"
+          />
+          <button className="p-2 mt-2 bg-cyan-700 rounded-md text-white hover:bg-cyan-600 transition-all duration-100">
+            Add to Cart
+          </button>
+        </div>
       </div>
-      <div className="flex-1">
-        <h2>
-          <span className="font-bold">Title:</span> {bookItem?.title}
-        </h2>
-        <h3>
-          <span className="font-bold">Subtitle:</span>
-          {bookItem?.subtitle}
-        </h3>
-        <p>
-          <span className="font-bold">ISBN:</span> {bookItem?.isbn13}
-        </p>
-        <p>
-          <span className="font-bold">Price:</span> {bookItem?.price}
-        </p>
-      </div>
-      <div className="flex flex-col items-end">
-        <input
-          className="border-2 border-cyan-700 rounded-md w-14 text-slate-950"
-          type="number"
-          name="cartcounter"
-          min="1"
-        />
-        <button className="p-2 mt-2 bg-cyan-700 rounded-md text-white hover:bg-cyan-600 transition-all duration-100">
-          Add to Cart
-        </button>
-      </div>
-    </div>
+    </Link>
   );
 }
 
