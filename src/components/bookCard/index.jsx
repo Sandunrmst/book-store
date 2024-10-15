03a3af70010d4298b904cfcf0df176b3e48709f2
sandunrmst <sandunalugc@gmail.com>
2024-10-15 09:55:51 +0530

    Display New Books

--- src/components/bookCard/index.jsx
+++ src/components/bookCard/index.jsx
@@ -1,22 +1,26 @@
-function BookCard() {
+function BookCard({ bookItem }) {
   return (
     <div className="w-full flex justify-evenly  border-2 rounded-md border-cyan-700 p-2 shadow-md gap-2">
       <div className="flex-1">
-        <img src="" alt="" className="object-cover w-full" />
+        <img
+          src={bookItem?.image}
+          alt={bookItem?.title}
+          className="object-cover w-full"
+        />
       </div>
       <div className="flex-1">
         <h2>
-          <span className="font-bold">Title:</span> Title
+          <span className="font-bold">Title:</span> {bookItem?.title}
         </h2>
         <h3>
           <span className="font-bold">Subtitle:</span>
-          Subtitle
+          {bookItem?.subtitle}
         </h3>
         <p>
-          <span className="font-bold">ISBN:</span> ISBN
+          <span className="font-bold">ISBN:</span> {bookItem?.isbn13}
         </p>
         <p>
-          <span className="font-bold">Price:</span> Price
+          <span className="font-bold">Price:</span> {bookItem?.price}
         </p>
       </div>
       <div className="flex flex-col items-end">
